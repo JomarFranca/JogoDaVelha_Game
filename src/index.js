4c1e57c65a30f90c3203174485dc1ca16a044cb7
Jomar A. França <jomar.almeida@gmail.com>
2022-06-07 10:22:21 -0300

    Configurando o Jogo de Xadrex

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import ReactDOM from "react-dom";
 
 const rowStyle = {
@@ -50,29 +51,66 @@ const buttonStyle = {
   fontSize: "16px",
 };
 
-function Square() {
-  return <div className="square" style={squareStyle}></div>;
+function Square({value1, value2, value3, jogar, pos}) {
+  let value = '';
+  switch (pos) {
+    case 1: value = value1; break;
+    case 2: value = value2; break;
+    case 3: value = value3; break;
+
+  }
+  return <div className="square" style={squareStyle} onClick={() => jogar(pos)} >{value}</div>;
 }
 
 function Board() {
+  const [nextPlayer, setNextPlayer] = useState('X');
+  const [value1, setValue1] = useState('');
+  const [value2, setValue2] = useState('');
+  const [value3, setValue3] = useState('');
+  const [winner, setWinner] = useState('');
+
+  const play = (pos) => {
+    console.log("vai jogar: ", pos, nextPlayer);
+    switch(pos) {
+      case 1: if(value1 === '') setValue1(nextPlayer); break;
+      case 2: if(value2 === '') setValue2(nextPlayer); break;
+      case 3: if(value3 === '') setValue3(nextPlayer); break;
+    }
+    if(pos === 1 && value1 === '' || (pos === 2 && value2 === '') || (pos === 3 && value3 === '')) {
+      setNextPlayer(nextPlayer === 'X' ? 'O' : 'X');
+    }
+
+    vencedor();
+  }
+
+  const limpar = () => {
+    console.log('limpou')
+    setNextPlayer("X");
+    setValue1('');
+    setValue2('');
+    setValue3('');
+  }
+
+  const vencedor = () => {    
+    if (value1 !== '' && value2 !== '' && value3 !== '') {
+      setWinner (nextPlayer);
+    }
+  }
+
   return (
     <div style={containerStyle} className="gameBoard">
       <div id="statusArea" className="status" style={instructionsStyle}>
-        Next player: <span>X</span>
+        Next player: <span>{nextPlayer}</span>
       </div>
-      {winner && (
-        <div id="winnerArea" className="winner" style={instructionsStyle}>
-          Winner: <span></span>
-        </div>
-      )}
-      <button style={buttonStyle} onClick={resetGame}>
-        Reset
-      </button>
+      <div id="winnerArea" className="winner" style={instructionsStyle}>
+        {winner && (<>Winner: <span>{winner}</span></>)}
+      </div>
+      <button style={buttonStyle} onClick={limpar}>Reset</button>
       <div style={boardStyle}>
         <div className="board-row" style={rowStyle}>
-          <Square />
-          <Square />
-          <Square />
+          <Square value1={value1} jogar={play} pos={1} />
+          <Square value2={value2} jogar={play} pos={2} />
+          <Square value3={value3} jogar={play} pos={3} />
         </div>
         <div className="board-row" style={rowStyle}>
           <Square />
